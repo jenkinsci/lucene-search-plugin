af07d100f9d5f019bf6b6dc39ca918317eb9ccb2
hanabishi <mcs@hanabi.se>
2014-08-23 20:45:47 +0200

    Added build status icon

--- src/main/java/org/jenkinsci/plugins/lucene/search/FreeTextSearchItemImplementation.java
+++ src/main/java/org/jenkinsci/plugins/lucene/search/FreeTextSearchItemImplementation.java
@@ -11,19 +11,23 @@ public class FreeTextSearchItemImplementation implements SearchItem {
 
     private final String buildNumber;
     private final String projectName;
+    private final String iconFileName;
     private final String[] bestFragments;
 
     public FreeTextSearchItemImplementation(final String projectName, final String buildNumber,
-            final String[] bestFragments) {
+            final String[] bestFragments, final String iconFileName) {
         this.projectName = projectName;
         this.buildNumber = buildNumber;
         this.bestFragments = bestFragments;
+        this.iconFileName = iconFileName;
     }
 
+    @Override
     public String getSearchUrl() {
         return Jenkins.getInstance().getRootUrl() + "/job/" + projectName + "/" + buildNumber + "/";
     }
 
+    @Override
     public String getSearchName() {
         return projectName + " #" + buildNumber;
     }
@@ -32,14 +36,21 @@ public class FreeTextSearchItemImplementation implements SearchItem {
         return bestFragments;
     }
 
+    public String getIconFileName() {
+        return iconFileName;
+    }
+
+    @Override
     public SearchIndex getSearchIndex() {
         return new SearchIndex() {
 
+            @Override
             public void suggest(final String token, final List<SearchItem> result) {
                 // TODO Auto-generated method stub
 
             }
 
+            @Override
             public void find(final String token, final List<SearchItem> result) {
                 // TODO Auto-generated method stub
 
