08a20af4a7f4e24f83c360c1751054886d1ea8ff
Tobias <tobias@olsson.be>
2015-03-10 22:09:30 +0100

    Fix so SearchModelObject searches are returned too
    SearchModelObject searches are what the default Jenkins implementation
    of search uses and returns. This should make that slightly better.
    
    Unfortunately, the search will only return 1(one) result even though
    the way I implemented it, it looks like it could return more. Multiple
    SearchModelObjects could theoretically return one result each, so I
    believe they are necessary.
    
    Developer Overview:
    Searching for user "foo" when both "foo" and "foo2" exists, will only
    yield "foo" in the SearchModelResults.

--- src/main/java/org/jenkinsci/plugins/lucene/search/FreeTextSearch.java
+++ src/main/java/org/jenkinsci/plugins/lucene/search/FreeTextSearch.java
@@ -1,18 +1,24 @@
 package org.jenkinsci.plugins.lucene.search;
 
 import hudson.search.Search;
+import hudson.search.SearchIndex;
+import hudson.search.SearchItem;
 import hudson.search.SearchResult;
+import hudson.search.SearchableModelObject;
+import hudson.search.SuggestedItem;
+import org.kohsuke.stapler.Ancestor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 
 public class FreeTextSearch extends Search {
     private final static Logger LOGGER = Logger.getLogger(Search.class.getName());
@@ -20,6 +26,7 @@ public class FreeTextSearch extends Search {
     private final SearchBackendManager manager;
 
     private List<FreeTextSearchItemImplementation> hits = Collections.emptyList();
+    private List<SearchItem> searchModelHits = Collections.emptyList();
 
     public FreeTextSearch(final SearchBackendManager manager) {
         this.manager = manager;
@@ -29,11 +36,46 @@ public class FreeTextSearch extends Search {
         return hits;
     }
 
+    public List<SearchItem> getSearchModelHits() {
+        return searchModelHits;
+    }
+
+    private List<SearchItem> normalSearch(StaplerRequest req, String query) {
+        List<SearchItem> searchResults = new ArrayList<SearchItem>() {
+            @Override
+            public Iterator<SearchItem> iterator() {
+                return super.iterator();
+            }
+        };
+
+        List<Ancestor> l = req.getAncestors();
+        for (int i = l.size() - 1; i >= 0; i--) {
+            Ancestor a = l.get(i);
+            if (a.getObject() instanceof SearchableModelObject) {
+                SearchableModelObject smo = (SearchableModelObject) a.getObject();
+                if (LOGGER.isLoggable(Level.FINE)) {
+                    LOGGER.fine(String.format("smo.displayName=%s, searchName=%s", smo.getDisplayName(), smo.getSearchName()));
+                }
+
+                SearchIndex index = smo.getSearchIndex();
+                SuggestedItem target = find(index, query, smo);
+                if (target != null) {
+                    searchResults.add(target.item);
+                }
+            }
+        }
+        return searchResults;
+    }
+
+    public boolean isEmptyResult() {
+        return hits.isEmpty() && searchModelHits.isEmpty();
+    }
+
     @Override
     public void doIndex(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
-        super.doIndex(req, rsp);
         String query = req.getParameter("q");
         if (query != null) {
+            searchModelHits = normalSearch(req, query);
             hits = manager.getHits(query, true);
         }
         req.getView(this, "search-results.jelly").forward(req, rsp);
