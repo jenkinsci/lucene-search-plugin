b3f961914985e2401b9267dbacc05a6990571df5
hanabishi <jenkins@hanabi.se>
2015-04-15 07:50:50 +0200

    Moved the rebuilddatabase method that is only used for tests to the CommonTestCase class. May need a better name for it but want to extract the test code from the production code

--- src/test/java/org/jenkinsci/plugins/lucene/search/databackend/CommonTestCases.java
+++ src/test/java/org/jenkinsci/plugins/lucene/search/databackend/CommonTestCases.java
@@ -1,18 +1,45 @@
 package org.jenkinsci.plugins.lucene.search.databackend;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import hudson.model.FreeStyleProject;
 import hudson.tasks.Shell;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
-import org.junit.Test;
+import org.jenkinsci.plugins.lucene.search.management.LuceneManager;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.xml.sax.SAXException;
 
 public class CommonTestCases {
+
+    public static void rebuildDatabase(final JenkinsSearchBackend jenkinsSearchBackend, JenkinsRule rule)
+            throws IOException, SAXException, InterruptedException, ExecutionException {
+        URL statusUrl = new URL(rule.getURL(), "lucenesearchmanager/status");
+        final URL rebuildUrl = new URL(rule.getURL(), "lucenesearchmanager/rebuildDatabase");
+
+        Future<Boolean> databaseRebuild = jenkinsSearchBackend.getBackgroundWorker().submit(new Callable<Boolean>() {
+
+            @Override
+            public Boolean call() throws Exception {
+                LuceneManager.JSReturnCollection jsonObject = jenkinsSearchBackend.getRebuildStatus(rebuildUrl);
+                assertEquals(0, jsonObject.code);
+                return true;
+            }
+        });
+        LuceneManager.JSReturnCollection jsonObject = jenkinsSearchBackend.getRebuildStatus(statusUrl);
+        while (jsonObject.running || jsonObject.neverStarted) {
+            Thread.sleep(1000);
+            jsonObject = jenkinsSearchBackend.getRebuildStatus(statusUrl);
+        }
+        assertEquals(0, jsonObject.code);
+        assertTrue("Something went wrong with rebuilding database", databaseRebuild.get());
+    }
 
     public static void givenSearchWhenJobsWithBuildsAreExecutedThenTheyShouldBeSearchable(
             JenkinsSearchBackend jenkinsSearchBackend, JenkinsRule rule) throws IOException, ExecutionException,
@@ -30,24 +57,29 @@ public class CommonTestCases {
         FreeStyleProject project3 = rule.createFreeStyleProject("project3");
         project3.getBuildersList().add(new Shell("cat $BUILD_TAG\n"));
         assertEquals(3, jenkinsSearchBackend.search("echo").suggestions.size());
-        jenkinsSearchBackend.rebuildDatabase();
+        rebuildDatabase(jenkinsSearchBackend, rule);
         assertEquals(3, jenkinsSearchBackend.search("echo").suggestions.size());
     }
 
     public static void givenSearchWhenIsNewItShouldSupportRebuildFromClean(JenkinsSearchBackend jenkinsSearchBackend,
             JenkinsRule rule) throws IOException, ExecutionException, InterruptedException, SAXException,
             URISyntaxException {
-        assertEquals(0, jenkinsSearchBackend.search("echo").suggestions.size());
-        jenkinsSearchBackend.rebuildDatabase();
-        assertEquals(0, jenkinsSearchBackend.search("echo").suggestions.size());
-        FreeStyleProject project1 = rule.createFreeStyleProject("project1");
-        project1.getBuildersList().add(new Shell("echo $BUILD_TAG\n"));
-        // Building
-        project1.scheduleBuild2(0).get();
-        project1.scheduleBuild2(0).get();
-        project1.scheduleBuild2(0).get();
-        jenkinsSearchBackend.rebuildDatabase();
-        assertEquals(3, jenkinsSearchBackend.search("echo").suggestions.size());
+        try {
+            assertEquals(0, jenkinsSearchBackend.search("echo").suggestions.size());
+            rebuildDatabase(jenkinsSearchBackend, rule);
+            assertEquals(0, jenkinsSearchBackend.search("echo").suggestions.size());
+            FreeStyleProject project1 = rule.createFreeStyleProject("project1");
+            project1.getBuildersList().add(new Shell("echo $BUILD_TAG\n"));
+            // Building
+            project1.scheduleBuild2(0).get();
+            project1.scheduleBuild2(0).get();
+            project1.scheduleBuild2(0).get();
+            rebuildDatabase(jenkinsSearchBackend, rule);
+            assertEquals(3, jenkinsSearchBackend.search("echo").suggestions.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
     }
 
 }
--- src/test/java/org/jenkinsci/plugins/lucene/search/databackend/JenkinsSearchBackend.java
+++ src/test/java/org/jenkinsci/plugins/lucene/search/databackend/JenkinsSearchBackend.java
@@ -1,9 +1,7 @@
 package org.jenkinsci.plugins.lucene.search.databackend;
 
 import com.google.common.io.Resources;
-import hudson.model.FreeStyleProject;
 import hudson.search.Search;
-import hudson.tasks.Shell;
 import jenkins.model.GlobalConfiguration;
 import net.sf.json.JSONObject;
 import org.jenkinsci.plugins.lucene.search.config.SearchBackendConfiguration;
@@ -13,17 +11,10 @@ import org.jvnet.hudson.test.JenkinsRule;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class JenkinsSearchBackend {
 
@@ -60,30 +51,13 @@ public class JenkinsSearchBackend {
         return list;
     }
 
-    public void rebuildDatabase() throws IOException, SAXException, InterruptedException, ExecutionException {
-        URL statusUrl = new URL(rule.getURL(), "lucenesearchmanager/status");
-        final URL rebuildUrl = new URL(rule.getURL(), "lucenesearchmanager/rebuildDatabase");
-        Future<Boolean> databaseRebuild = backgroundWorker.submit(new Callable<Boolean>() {
-
-            @Override
-            public Boolean call() throws Exception {
-                LuceneManager.JSReturnCollection jsonObject = getRebuildStatus(rebuildUrl);
-                assertEquals(0, jsonObject.code);
-                return true;
-            }
-        });
-        LuceneManager.JSReturnCollection jsonObject = getRebuildStatus(statusUrl);
-        while (jsonObject.running || jsonObject.neverStarted) {
-            Thread.sleep(1000);
-            jsonObject = getRebuildStatus(statusUrl);
-        }
-        assertEquals(0, jsonObject.code);
-        assertTrue("Something went wrong with rebuilding database", databaseRebuild.get());
-    }
-
     public LuceneManager.JSReturnCollection getRebuildStatus(URL url) throws IOException {
         String jsonString = Resources.toString(url, Charset.defaultCharset());
         return (LuceneManager.JSReturnCollection) JSONObject.fromObject(jsonString).toBean(
                 LuceneManager.JSReturnCollection.class);
+    }
+
+    public ExecutorService getBackgroundWorker() {
+        return backgroundWorker;
     }
 }
