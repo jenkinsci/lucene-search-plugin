27e1f91a7a979f0e2c8571072469a085053760d1
Mark Chen <39845648+hmarkc@users.noreply.github.com>
2021-09-01 09:47:03 +0800

    Update FreeTextSearchItem.java

--- src/main/java/org/jenkinsci/plugins/lucene/search/FreeTextSearchItem.java
+++ src/main/java/org/jenkinsci/plugins/lucene/search/FreeTextSearchItem.java
@@ -4,15 +4,7 @@ import hudson.search.SearchItem;
 import jenkins.model.Jenkins;
 
 public abstract class FreeTextSearchItem implements SearchItem {
-
-    /**
-     * Turns the searchUrl into a url or path depending on if the searchUrl
-     * starts with a '/'.
-     * But the relative url will be from <ROOT> of jenkins but when we just use the relative url 
-     * it will append it to the current URL <ROOT>/search.
-     * 
-     * @return something suitable for inserting into an href
-     */
+    
     public String getUrl() {
         String root = Jenkins.getInstance().getRootUrl();
         String searchUrl = getSearchUrl();
