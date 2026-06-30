ba37c39c1274b6960a3b6cd306cf832658822d83
Michael Trimarchi <michael@amarulasolutions.com>
2026-06-30 22:10:34 +0200

    refactor(FreeTextRunListener): switch from log4j to java.util.logging
    Convert Logger to java.util.logging, rename variable to LOG,
    map debug() → fine(), error() → log(Level.SEVERE).
    
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

--- src/main/java/org/jenkinsci/plugins/lucene/search/FreeTextRunListener.java
+++ src/main/java/org/jenkinsci/plugins/lucene/search/FreeTextRunListener.java
@@ -7,54 +7,55 @@ import hudson.model.TaskListener;
 import hudson.model.listeners.RunListener;
 import java.io.IOException;
 import javax.inject.Inject;
-import org.apache.log4j.Logger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jenkinsci.plugins.lucene.search.databackend.SearchBackendManager;
 
 @Extension
 public class FreeTextRunListener extends RunListener<Run<?, ?>> {
 
-    Logger logger = Logger.getLogger(FreeTextRunListener.class);
+    private static final Logger LOG = Logger.getLogger(FreeTextRunListener.class.getName());
 
     @Inject
     SearchBackendManager searchBackendManager;
 
     @Override
     public void onStarted(final Run<?, ?> build, @NonNull final TaskListener listener) {
-        logger.debug("onStarted: storing build=" + build.getFullDisplayName()
+        LOG.fine("onStarted: storing build=" + build.getFullDisplayName()
                 + " number=" + build.getNumber()
                 + " isBuilding=" + build.isBuilding()
                 + " isLogUpdated=" + build.isLogUpdated());
         try {
             searchBackendManager.storeBuild(build);
-            logger.debug("onStarted: stored OK build=" + build.getFullDisplayName());
+            LOG.fine("onStarted: stored OK build=" + build.getFullDisplayName());
         } catch (IOException e) {
-            logger.error("When saving the started build index: ", e);
+            LOG.log(Level.SEVERE, "When saving the started build index: ", e);
         }
     }
 
     @Override
     public void onCompleted(final Run<?, ?> build, @NonNull final TaskListener listener) {
-        logger.debug("onCompleted: storing build=" + build.getFullDisplayName()
+        LOG.fine("onCompleted: storing build=" + build.getFullDisplayName()
                 + " number=" + build.getNumber()
                 + " result=" + build.getResult()
                 + " duration=" + build.getDuration());
         try {
             searchBackendManager.storeBuild(build);
-            logger.debug("onCompleted: stored OK build=" + build.getFullDisplayName());
+            LOG.fine("onCompleted: stored OK build=" + build.getFullDisplayName());
         } catch (IOException e) {
-            logger.error("When saving the finished build index: ", e);
+            LOG.log(Level.SEVERE, "When saving the finished build index: ", e);
         }
     }
 
     @Override
     public void onDeleted(final Run<?, ?> build) {
-        logger.debug("onDeleted: removing build=" + build.getFullDisplayName()
+        LOG.fine("onDeleted: removing build=" + build.getFullDisplayName()
                 + " number=" + build.getNumber());
         try {
             searchBackendManager.removeBuild(build);
-            logger.debug("onDeleted: removed OK build=" + build.getFullDisplayName());
+            LOG.fine("onDeleted: removed OK build=" + build.getFullDisplayName());
         } catch (IOException e) {
-            logger.error("When removing the deleted build index: ", e);
+            LOG.log(Level.SEVERE, "When removing the deleted build index: ", e);
         }
     }
 }
